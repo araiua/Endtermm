7ceac0fd5fcc7328187a86d3c937b12f594f0d61
cuchj0 <arai.uakit@mail.ru>
2026-02-15 03:02:45 +0500

    c

--- src/ConnectionOfData.java
+++ src/ConnectionOfData.java
@@ -1,2 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionOfData {
+    private static final String url = "jdbc:postgresql://localhost:5432/endterm_oop";
+    private static final String user = "postgres";
+    private static final String password = "13062013";
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
